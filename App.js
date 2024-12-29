5b6bc5d0ec5d79d70a0998c7bad87b1527e51297
Taisei <taisei.960420@gmail.com>
2024-12-29 21:39:11 +0900

    コンポーネントの作成

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, SafeAreaView, FlatList } from 'react-native';
+import { ListItem } from './components/ListItem';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Constants from 'expo-constants';
+
+const URL = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${Constants.expoConfig.extra.newsApiKey}`;
 
 export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  const fetchArticles = async () => {
+    try {
+      const response = await axios.get(URL);
+      console.log('ニュースを取得', response.data);
+      setArticles(response.data.articles);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // 画面の初期化でデータを取得
+    fetchArticles();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={articles}
+        renderItem={({ item }) => {
+          return <ListItem imageUrl={item.urlToImage} title={item.title} author={item.author} />;
+        }}
+        keyExtractor={(item, index) => index.toString()}
+      />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
